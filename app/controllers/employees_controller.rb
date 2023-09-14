c4d65a4b969e1dad0edb47851064ff4f5190b241
Gopal Jhade <gopaljhade@gmail.com>
2023-09-14 15:22:26 +0530

    CURD operation perform and also view config

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,10 +1,11 @@
 class EmployeesController < ApplicationController
+
+  before_action :set_employee, only: [:edit, :update, :show, :destroy]
   def index
     @employees = Employee.all
   end
 
   def show
-    @employee = Employee.find(params[:id])
   end
 
   def new
@@ -20,22 +21,31 @@ class EmployeesController < ApplicationController
     end
   end
 
-  # def edit
-  #   @employee = Employee.find(params[:id])
-  # end
+  def edit  
+  end
 
-  # def update
-  #   @employee = Employee.find(params[:id])
+  def update
+    if @employee.update(employee_params)
+      redirect_to employees_path, notice: 'Employee has been updated successfully'
+    else
+      render :edit
+    end
+  end
 
-  #   if @employee.update(employee_params)
-  #     redirect_to @employee
-  #   else
-  #     render :edit, status: :unprocessable_entity
-  #   end
-  # end
+  def destroy
+    if @employee.destroy
+      redirect_to employees_path, notice: "employee has been deleted successfully"
+    end
+  end
 
   private
-    def employee_params
-      params.require(:employee).permit(:first_name, :middle_name, :last_name, :personal_email, :city, :state, :country, :pincode, :address_line_1, :address_line_2)
-    end
+  def employee_params
+    params.require(:employee).permit(:first_name, :middle_name, :last_name, :personal_email, :city, :state, :country, :pincode, :address_line_1, :address_line_2)
+  end
+
+  def set_employee
+      @employee = Employee.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => error
+    redirect_to employees_path, notice: error
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+
+10.times do |index|
+  Employee.create(
+    first_name: "First Name#{index + 1}",
+    last_name: "Last Name#{index + 1}",
+    personal_email: "employee#{index + 1}@example.com",
+    city: "City#{rand(1..3)}",
+    state: "State#{rand(1..3)}",
+    address_line_1: "Address Line 1#{index + 1}",
+    country: "Country#{rand(1..3)}",
+    pincode: "#{rand(10000..99999)}"
+  )
+end
