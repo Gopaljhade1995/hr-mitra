ed0965db6fbb48911567b73760629a616e37ab61
Gopal Jhade <gopaljhade@gmail.com>
2023-09-15 19:45:16 +0530

    use to devise gem for authenticate user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
+   }
+
  resources :employees
  resources :documents
  get 'about' => 'pages#about_us'
